d8212f2133791ce05fa10d2f757aa81fc6ed4a55
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-17 13:49:42 +0600

    Added codes

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    env: {
+      commonjs: true,
+      es6: true,
+      node: true,
+    },
+    extends: [
+      'airbnb-base',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+      describe: true,
+      it: true,
+      test: true,
+      expect: true,
+      beforeAll: true,
+      beforeEach: true,
+      afterEach: true,
+      afterAll: true,
+    },
+    parserOptions: {
+      ecmaVersion: 2017,
+    },
+    rules: {
+      "indent": ["error", "tab"],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "no-tabs": "off",
+      "no-mixed-spaces-and-tabs": "error",
+      "no-console": "off",
+      "global-require": "off",
+      "import/no-dynamic-require": "off",
+      "class-methods-use-this": "off",
+      "radix": ["error", "as-needed"],
+      "func-names": "off",
+      "max-len": ["error", { "code": 150 }],
+      "prefer-arrow-callback": "off",
+      "no-useless-escape": "off",
+      "no-unneeded-ternary": "off",
+      "no-restricted-properties": "off",
+    },
+  };
+  
