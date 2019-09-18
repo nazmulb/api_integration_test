855dc9a72fc7486acd6541800196236491a890c6
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-18 21:30:10 +0600

    Added DB models

---
+++ config/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 80,
+    JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'abc123',
+    SALT_ROUNDS: process.env.SALT_ROUNDS || 10,
+    DB_CONFIG: require('./db.config')
+  };
--- models/index.js
+++ models/index.js
@@ -4,15 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config');
+const dbConfig = config.DB_CONFIG[config.ENV];
+
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (dbConfig.url) {
+  sequelize = new Sequelize(dbConfig.url, dbConfig);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 }
 
 fs
