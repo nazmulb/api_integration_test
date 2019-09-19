f489f8353d98b4feb03d27dc0ed0ee9f23161c26
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-19 13:01:49 +0600

    Added DB models

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     ENV: process.env.NODE_ENV || 'development',
+    BASE_URL: process.env.BASE_URL || "http://localhost",
     PORT: process.env.PORT || 80,
     JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'abc123',
     SALT_ROUNDS: process.env.SALT_ROUNDS || 10,
