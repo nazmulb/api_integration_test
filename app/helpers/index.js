52d3a0f67df2b92676121b8dea5a73b66be81494
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-19 15:01:28 +0600

    added codes

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -1,24 +1,7 @@
 /**
-* Common Utility Methods
-*/
+ * Common Helper Classes
+ */
 
 module.exports = {
-	/**
-    * Count letter
-    * @param {string} str - given string
-    * @param {string} char - given character
-    * @return {number} count - no of characters
-    * @example
-    *      helper.letterCount("test", "t");
-    */
-	letterCount(str, char) {
-		let count = 0;
-		for (let i = 0; i <= str.length; i += 1) {
-			if (str[i] === char) {
-				count += 1;
-			}
-		}
-
-		return count;
-	},
+	Utility: require("./Utility"),
 };
