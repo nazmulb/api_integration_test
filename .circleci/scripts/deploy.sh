38b3bf8adb73542de616285efa422546cd5a6474
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-27 22:09:07 +0600

    added codes

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 
-cd ~/app
 docker-compose -f docker-compose-deploy.yml up -d
 sleep 5
-docker-compose -f docker-compose-deploy.yml exec node bash -c "npm run db && npm run migrate && npm run seed"
+docker-compose -f docker-compose-deploy.yml exec node bash -c "cd ~/app && npm run db && npm run migrate && npm run seed"
