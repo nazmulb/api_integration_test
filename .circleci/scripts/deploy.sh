542cdc27fe49553a216dd0e4ea22869eb2dbb4d0
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-27 21:30:10 +0600

    added codes

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 
-docker pull nazmulb/api_integration_test:latest
 docker-compose up -d
 sleep 5
 docker-compose exec node bash -c "npm run db && npm run migrate && npm run seed"
