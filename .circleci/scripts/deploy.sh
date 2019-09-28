407355bda864448982ca92406100497fcf416bee
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-28 13:37:13 +0600

    added codes

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -9,5 +9,6 @@ docker run -d -p 8082:8082 --name node_server --link mysql_server:mysql -e DEV_D
 sleep 5
 docker ps
 curl http://localhost:8082/api/about
-npm run db && npm run migrate && npm run seed
+TEST_DATABASE_URL=mysql://root:123@localhost:3306/apimicro_test npm run db
+#npm run db && npm run migrate && npm run seed
 #docker-compose -f docker-compose-deploy.yml exec app bash -c "npm run db && npm run migrate && npm run seed"
