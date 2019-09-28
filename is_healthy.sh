bd7392d6ee1f2573d36f28659239f26a5e58db78
nazmulbasher <nazmul.basher@fieldnation.com>
2019-09-28 14:48:52 +0600

    added codes

--- is_healthy.sh
+++ is_healthy.sh
@@ -3,4 +3,4 @@ set -ex
 
 export AUTH=$(curl -d "email=nazmul.basher@gmail.com&password=123" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:8082/api/auth)
 
-curl http://localhost:8082/api/users -v -H "auth_token: $AUTH" -X GET
+curl http://localhost:8082/api/users -v -H "auth_token: $AUTH"
